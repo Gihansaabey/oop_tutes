316550272e502c23d36b9cebd2e758fb522283e3
Gihansaabey <138303685+Gihansaabey@users.noreply.github.com>
2023-11-05 21:50:54 +0530

    created class person

--- src/week_1/Main.java
+++ src/week_1/Main.java
@@ -1,5 +1,40 @@
 package week_1;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Person women = new Person("Gihansa");
+        women.displayName();
+
+        women.setSurname("abeygunawardena");
+        women.setAge(21);
+        String surName = women.getSurname();
+        System.out.println(surName);
+        int age = women.getAge();
+        System.out.println(age);
+        System.out.println();
+
+        Person women_two = new Person("amaya");
+        women_two.displayName();
+
+        women_two.setSurname("wijesinghe");
+        women_two.setAge(20);
+        surName = women_two.getSurname();
+        System.out.println(surName);
+        age = women_two.getAge();
+        System.out.println(age);
+        System.out.println();
+
+        Person women_three = new Person("piushana");
+        women_three.displayName();
+
+        women_three.setSurname("kumudanjali");
+        women_three.setAge(18);
+        surName = women_three.getSurname();
+        System.out.println(surName);
+        age = women_three.getAge();
+        System.out.println(age);
+
+    }
 
 }
--- src/week_1/Person.java
+++ src/week_1/Person.java
@@ -1,5 +1,31 @@
 package week_1;
 
 public class Person {
+    private String name;
+    private String surname;
+    private int age;
 
+    public Person(String nam){
+        name=nam;
+    }
+    public void displayName(){
+        System.out.println(name);
+    }
+
+    public void  setSurname(String sName){
+        surname=sName;
+    }
+    public void setAge(int mAge){
+        age=mAge;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+    public int getAge(){
+        return age;
+    }
 }
+
+
+
